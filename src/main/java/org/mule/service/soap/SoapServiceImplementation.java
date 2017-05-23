3356923c29dfb15537897835b7d6073394e4e4e3
Rodrigo <elrodro83@users.noreply.github.com>
2017-05-23 18:32:42 -0300

    MULE-12406: HTTP, Oauth and Soap services have wrong package name (#5)

---
+++ src/main/java/org/mule/service/soap/SoapServiceImplementation.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.service.soap;
+
+import org.mule.runtime.soap.api.SoapService;
+import org.mule.runtime.soap.api.client.SoapClientFactory;
+import org.mule.service.soap.client.SoapCxfClientFactory;
+
+/**
+ * Default Mule {@link SoapService} implementation.
+ *
+ * @since 1.0
+ */
+public class SoapServiceImplementation implements SoapService {
+
+  @Override
+  public String getName() {
+    return "SOAP Service";
+  }
+
+  @Override
+  public SoapClientFactory getClientFactory() {
+    return new SoapCxfClientFactory();
+  }
+}
