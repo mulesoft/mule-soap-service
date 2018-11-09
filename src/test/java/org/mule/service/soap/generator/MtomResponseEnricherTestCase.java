2385a8da65747dd08bf53b03d7952d5cca69b7ab
bardammu <juan.baca@mulesoft.com>
2018-11-09 08:36:40 -0300

    Add test

--- src/test/java/org/mule/service/soap/generator/MtomResponseEnricherTestCase.java
+++ src/test/java/org/mule/service/soap/generator/MtomResponseEnricherTestCase.java
@@ -6,36 +6,48 @@
  */
 package org.mule.service.soap.generator;
 
-import org.mule.metadata.xml.api.XmlTypeLoader;
+import static org.mule.service.soap.SoapTestUtils.assertSimilarXml;
+import static org.mule.service.soap.SoapTestXmlValues.DOWNLOAD_ATTACHMENT;
+import static org.mule.service.soap.util.XmlTransformationUtils.stringToDocument;
+
 import org.mule.service.soap.generator.attachment.AttachmentResponseEnricher;
 import org.mule.service.soap.generator.attachment.MtomResponseEnricher;
 import org.mule.wsdl.parser.model.operation.OperationModel;
 
 import java.util.Map;
 
-import org.apache.cxf.message.Exchange;
+import io.qameta.allure.Description;
+import org.apache.cxf.message.ExchangeImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.w3c.dom.Document;
 
-public class MtomResponseEnricherTestCase extends ResponseEnricherTestCase {
+public class MtomResponseEnricherTestCase extends AbstractEnricherTestCase {
 
-  private static final String RESPONSE =
-      "<con:downloadAttachmentResponse xmlns:con=\"http://service.soap.service.mule.org/\">"
-          + "<attachment>"
-          + "<xop:Include xmlns:xop=\"http://www.w3.org/2004/08/xop/include\" href=\"cid:attachment-id\"/>"
-          + "</attachment>"
-          + "</con:downloadAttachmentResponse>";
+  private AttachmentResponseEnricher enricher;
 
-  @Override
-  protected AttachmentResponseEnricher getEnricher(XmlTypeLoader loader, Map<String, OperationModel> ops) {
-    return new MtomResponseEnricher(loader, ops);
+  @Before
+  public void setup() {
+    super.setup();
+    Map<String, OperationModel> ops = model.getService("TestService").getPort("TestPort").getOperationsMap();
+    enricher = new MtomResponseEnricher(model.getLoader().getValue(), ops);
   }
 
-  @Override
-  protected String getResponse() {
-    return RESPONSE;
-  }
+  @Test
+  @Description("Enrich a response that contains attachments")
+  public void enrich() throws Exception {
+    String response =
+        "<con:downloadAttachmentResponse xmlns:con=\"http://service.soap.service.mule.org/\">"
+            + "<attachment>"
+            + "<xop:Include xmlns:xop=\"http://www.w3.org/2004/08/xop/include\" href=\"cid:attachment-id\"/>"
+            + "</attachment>"
+            + "</con:downloadAttachmentResponse>";
 
-  @Override
-  protected void assertAttachment(Exchange exchange) {
-    // Attachments are handled by the Mtom Interceptor
+    ExchangeImpl exchange = new ExchangeImpl();
+    Document doc = stringToDocument(response);
+
+    String result = enricher.enrich(doc, DOWNLOAD_ATTACHMENT, exchange);
+
+    assertSimilarXml(testValues.getDownloadAttachmentResponse(), result);
   }
 }
